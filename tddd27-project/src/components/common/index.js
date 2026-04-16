91847b6b8fb6dc6de02a2e0573449ceaada6f02d
De99 <Deema.abogheda@gmail.com>
2026-04-16 13:57:12 +0200

    Move login logic into a component

--- tddd27-project/src/components/common/index.js
+++ tddd27-project/src/components/common/index.js
@@ -1,11 +1,13 @@
 import Header from "./header";
 import Navbar from "./navbar";
 import Course from "./course";
+import Login from "./login/Login";
 
 
 
 export {
     Header,
+    Login,
     Navbar,
     Course,
 }
