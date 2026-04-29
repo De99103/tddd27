efbde46291403c5339ab27d775fe165ff48a9521
Emma Davidsson <davidsson470@gmail.com>
2026-04-29 11:51:38 +0200

    Fix
    Gjort en popup för kurserna med placeholder där variabler för kursnamn, etc ska stå.
    
    Har lagt till en component-mall, så att det blir lättare att skapa nya.
    
    Ska påbörja en komponent för betygsättnign av kursen. Klickbara sifftor som ska få en ring runt sig när de är valda. Max en kan vara valt samtidigt och statistik ska kunan göras baserat på det.

--- tddd27-project/src/components/common/popup/Popup.jsx
+++ tddd27-project/src/components/common/popup/Popup.jsx
@@ -1,11 +1,48 @@
 import Autocomplete from "../autocomplete/Autocomplete";
 import "./Popup.css";
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
 
 function Popup() {
+    const [courseRated, drawCircle] = useState(false);
+    const toggleCircle = () => {
+        drawCircle(!courseRated);
+        console.log("Du försökte rate:a");
+    };
+
     return (
-        <div className="page-popup">
-            <div className="container-popup"></div>
-            <h1>Namn på kurs</h1>
+        <div className="page_popup">
+            <div className="container_popup">
+                <div className="top_of_post_it">
+                    <p className="popup_course_code">TNG032</p>
+                </div>
+                <p className="popup_course_name">Applied Transform Theory</p>
+                <div className="examinator_and_department">
+                    <p className="popup_examinator">Examinator: Lukás Malý</p>
+                    <p className="popup_department">Department: ITN</p>
+                </div>
+                <div className="popup_grade_container">
+                    <p className="popup_grade">Grade: </p>
+                    <p className="popup_grade_variable">3</p>
+                </div>
+                <p className="text_before_rating"> My rating of the course:</p>
+                <div className="popup_rate_course">
+                    {/* byt ut mot komponenter */}
+                    <p
+                        className="popup_rate_2"
+                        onClick={toggleCircle}
+                        isOpen={courseRated}
+                    >
+                        1
+                    </p>
+                    <p className="popup_rate_2">2</p>
+                    <p className="popup_rate_2">3</p>
+                    <p className="popup_rate_2">4</p>
+                    <p className="popup_rate_2">5</p>
+                </div>
+
+                <p className="rate_course_rating_selected">O</p>
+            </div>
         </div>
     );
 }
